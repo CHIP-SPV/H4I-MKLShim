042d75d96ee941c255df38399e1bed22a6eeff93
Sarbojit Sarkar <sarbojit.sarkar@intel.com>
2023-02-01 13:15:10 +0530

    Added all level-1 primitives

--- include/h4i/mklshim/onemklblas.h
+++ include/h4i/mklshim/onemklblas.h
@@ -4,6 +4,19 @@
 
 namespace H4I::MKLShim
 {
+  void sDot(Context* ctxt, int64_t n, const float *x, int64_t incx, const float *y,
+            int64_t incy, float *result);
+  void dDot(Context* ctxt, int64_t n, const double *x, int64_t incx, const double *y,
+            int64_t incy, double *result);
+  void cDotc(Context* ctxt, int64_t n, const float _Complex *x, int64_t incx,
+             const float _Complex *y, int64_t incy, float _Complex *result);
+  void zDotc(Context* ctxt, int64_t n, const double _Complex *x, int64_t incx,
+             const double _Complex *y, int64_t incy, double _Complex *result);
+  void cDotu(Context* ctxt, int64_t n, const float _Complex *x, int64_t incx,
+             const float _Complex *y, int64_t incy, float _Complex *result);
+  void zDotu(Context* ctxt, int64_t n, const double _Complex *x, int64_t incx,
+             const double _Complex *y, int64_t incy, double _Complex *result);
+
   void sAsum(Context* ctxt, int64_t n, const float *x, int64_t incx,
              float *result);
   void dAsum(Context* ctxt, int64_t n, const double *x, int64_t incx,
@@ -22,6 +35,33 @@ namespace H4I::MKLShim
   void zAxpy(Context* ctxt, int64_t n, double _Complex alpha, const double _Complex *x,
              int64_t incx, double _Complex *y, int64_t incy);
 
+  void dScal(Context* ctxt, int64_t n, double alpha, double *x, int64_t incx);
+  void sScal(Context* ctxt, int64_t n, float alpha, float *x, int64_t incx);
+  void cScal(Context* ctxt, int64_t n, float _Complex alpha, float _Complex *x,
+              int64_t incx);
+  void csScal(Context* ctxt, int64_t n, float alpha, float _Complex *x, int64_t incx);
+  void zsScal(Context* ctxt, int64_t n, double _Complex alpha, double _Complex *x,
+              int64_t incx);
+  void zdScal(Context* ctxt, int64_t n, double alpha, double _Complex *x, int64_t incx);
+
+  void dNrm2(Context* ctxt, int64_t n, const double *x, int64_t incx,
+              double *result);
+  void sNrm2(Context* ctxt, int64_t n, const float *x,int64_t incx,
+              float *result);
+  void cNrm2(Context* ctxt, int64_t n, const float _Complex *x, int64_t incx,
+              float *result);
+  void zNrm2(Context* ctxt, int64_t n, const double _Complex *x, int64_t incx,
+              double *result);
+
+  void dCopy(Context* ctxt, int64_t n, const double *x,int64_t incx,
+              double *y, int64_t incy);
+  void sCopy(Context* ctxt, int64_t n, const float *x,int64_t incx,
+        float *y, int64_t incy);
+  void zCopy(Context* ctxt, int64_t n, const double _Complex *x,
+              int64_t incx, double _Complex *y, int64_t incy);
+  void cCopy(Context* ctxt, int64_t n, const float _Complex *x,
+              int64_t incx, float _Complex *y, int64_t incy);
+
   void sAmax(Context* ctxt, int64_t n, const float  *x, int64_t incx,
               int64_t *result);
   void dAmax(Context* ctxt, int64_t n, const double *x, int64_t incx,
@@ -30,4 +70,40 @@ namespace H4I::MKLShim
               int64_t *result);
   void zAmax(Context* ctxt, int64_t n, const double _Complex *x, int64_t incx,
               int64_t *result);
+
+  void dAmin(Context* ctxt, int64_t n, const double *x, int64_t incx, int64_t *result);
+  void sAmin(Context* ctxt, int64_t n, const float  *x, int64_t incx, int64_t *result);
+  void zAmin(Context* ctxt, int64_t n, const double _Complex *x, int64_t incx, int64_t *result);
+  void cAmin(Context* ctxt, int64_t n, const float _Complex *x, int64_t incx, int64_t *result);
+
+  void sSwap(Context* ctxt, int64_t n, float *x, int64_t incx, float *y, int64_t incy);
+  void dSwap(Context* ctxt, int64_t n, double *x, int64_t incx, double *y, int64_t incy);
+  void cSwap(Context* ctxt, int64_t n, float _Complex *x, int64_t incx,
+              float _Complex *y, int64_t incy);
+  void zSwap(Context* ctxt, int64_t n, double _Complex *x, int64_t incx,
+              double _Complex *y, int64_t incy);
+
+  void sRot(Context* ctxt, int n, float* x, int incx, float* y, int incy,
+              const float c, const float s);
+  void dRot(Context* ctxt, int n, double* x, int incx, double* y, int incy,
+              const double c, const double s);
+  void cRot(Context* ctxt, int n, float _Complex* x, int incx, float _Complex* y, int incy,
+              const float c, const float _Complex s);
+  void csRot(Context* ctxt, int n, float _Complex* x, int incx, float _Complex* y, int incy,
+              const float c, const float s);
+  void zRot(Context* ctxt, int n, double _Complex* x, int incx, double _Complex* y, int incy,
+              const double c, const double _Complex s);
+  void zdRot(Context* ctxt, int n, double _Complex* x, int incx, double _Complex* y, int incy,
+              const double c, const double s);
+
+  void sRotg(Context* ctxt, float* a, float* b, float* c, float* s);
+  void dRotg(Context* ctxt, double* a, double* b, double* c, double* s);
+  void cRotg(Context* ctxt, float _Complex* a, float _Complex* b, float* c, float _Complex* s);
+  void zRotg(Context* ctxt, double _Complex* a, double _Complex* b, double* c, double _Complex* s);
+
+  void sRotm(Context* ctxt, int64_t n, float *x, int64_t incx, float *y, int64_t incy, float* param);
+  void dRotm(Context* ctxt, int64_t n, double *x, int64_t incx, double *y, int64_t incy, double* param);
+
+  void sRotmg(Context* ctxt, float *d1, float *d2, float *x1, float y1, float* param);
+  void dRotmg(Context* ctxt, double *d1, double *d2, double *x1, double y1, double* param);
 } // namespace
--- src/onemklblas.cpp
+++ src/onemklblas.cpp
@@ -10,6 +10,63 @@
 
 namespace H4I::MKLShim
 {
+  //dot
+  void sDot(Context* ctxt, int64_t n, const float *x, int64_t incx, const float *y,
+            int64_t incy, float *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::dot(ctxt->queue, n, x,
+                                                      incx, y, incy, result);
+    ONEMKL_CATCH("DOT")
+  }
+
+  void dDot(Context* ctxt, int64_t n, const double *x, int64_t incx, const double *y,
+            int64_t incy, double *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::dot(ctxt->queue, n, x,
+                                                      incx, y, incy, result);
+    ONEMKL_CATCH("DOT")
+  }
+
+  void cDotc(Context* ctxt, int64_t n, const float _Complex *x, int64_t incx,
+             const float _Complex *y, int64_t incy, float _Complex *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::dotc(ctxt->queue, n,
+                                                reinterpret_cast<const std::complex<float> *>(x), incx,
+                                                reinterpret_cast<const std::complex<float> *>(y), incy,
+                                                reinterpret_cast<std::complex<float> *>(result));
+    ONEMKL_CATCH("DOT")
+  }
+
+  void zDotc(Context* ctxt, int64_t n, const double _Complex *x, int64_t incx,
+             const double _Complex *y, int64_t incy, double _Complex *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::dotc(ctxt->queue, n,
+                                                reinterpret_cast<const std::complex<double> *>(x), incx,
+                                                reinterpret_cast<const std::complex<double> *>(y), incy,
+                                                reinterpret_cast<std::complex<double> *>(result));
+    ONEMKL_CATCH("DOT")
+  }
+
+  void cDotu(Context* ctxt, int64_t n, const float _Complex *x, int64_t incx,
+             const float _Complex *y, int64_t incy, float _Complex *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::dotu(ctxt->queue, n,
+                                                reinterpret_cast<const std::complex<float> *>(x), incx,
+                                                reinterpret_cast<const std::complex<float> *>(y), incy,
+                                                reinterpret_cast<std::complex<float> *>(result));
+    ONEMKL_CATCH("DOT")
+  }
+
+  void zDotu(Context* ctxt, int64_t n, const double _Complex *x, int64_t incx,
+             const double _Complex *y, int64_t incy, double _Complex *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::dotu(ctxt->queue, n,
+                                                reinterpret_cast<const std::complex<double> *>(x), incx,
+                                                reinterpret_cast<const std::complex<double> *>(y), incy,
+                                                reinterpret_cast<std::complex<double> *>(result));
+    ONEMKL_CATCH("DOT")
+  }
+
   //asum
   void sAsum(Context* ctxt, int64_t n, const float *x, int64_t incx,
             float *result) {
@@ -80,6 +137,126 @@ namespace H4I::MKLShim
     ONEMKL_CATCH("AXPY")
   }
 
+  //scal
+  void dScal(Context* ctxt, int64_t n, double alpha,
+                    double *x, int64_t incx) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::scal(ctxt->queue, n, alpha,
+                                                    x, incx);
+    ONEMKL_CATCH("SCAL")
+
+  }
+
+  void sScal(Context* ctxt, int64_t n, float alpha,
+                              float *x, int64_t incx) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::scal(ctxt->queue, n, alpha,
+                                                        x, incx);
+    ONEMKL_CATCH("SCAL")
+  }
+
+  void cScal(Context* ctxt, int64_t n,
+                              float _Complex alpha, float _Complex *x,
+                              int64_t incx) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::scal(ctxt->queue, n,
+                                        static_cast<std::complex<float> >(alpha),
+                                        reinterpret_cast<std::complex<float> *>(x),incx);
+    ONEMKL_CATCH("SCAL")
+  }
+
+  void csScal(Context* ctxt, int64_t n,
+                              float alpha, float _Complex *x,
+                              int64_t incx) {
+      ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::scal(ctxt->queue, n, alpha,
+                                          reinterpret_cast<std::complex<float> *>(x),incx);
+      ONEMKL_CATCH("SCAL")
+  }
+
+  void zsScal(Context* ctxt, int64_t n,
+                              double _Complex alpha, double _Complex *x,
+                              int64_t incx) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::scal(ctxt->queue, n,
+                                        static_cast<std::complex<double> >(alpha),
+                                        reinterpret_cast<std::complex<double> *>(x),incx);
+    ONEMKL_CATCH("SCAL")
+  }
+
+  void zdScal(Context* ctxt, int64_t n,
+                              double alpha, double _Complex *x,
+                              int64_t incx) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::scal(ctxt->queue, n, alpha,
+                                        reinterpret_cast<std::complex<double> *>(x),incx);
+    ONEMKL_CATCH("SCAL")
+  }
+
+  //nrm2
+  void dNrm2(Context* ctxt, int64_t n, const double *x,
+                              int64_t incx, double *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::nrm2(ctxt->queue, n, x, incx, result);
+    ONEMKL_CATCH("NRM2")
+  }
+
+  void sNrm2(Context* ctxt, int64_t n, const float *x,
+                              int64_t incx, float *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::nrm2(ctxt->queue, n, x, incx, result);
+    ONEMKL_CATCH("NRM2")
+  }
+
+  void cNrm2(Context* ctxt, int64_t n, const float _Complex *x,
+                              int64_t incx, float *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::nrm2(ctxt->queue, n,
+                    reinterpret_cast<const std::complex<float> *>(x), incx, result);
+    ONEMKL_CATCH("NRM2")
+  }
+
+  void zNrm2(Context* ctxt, int64_t n, const double _Complex *x,
+                              int64_t incx, double *result) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::nrm2(ctxt->queue, n,
+                    reinterpret_cast<const std::complex<double> *>(x), incx, result);
+    ONEMKL_CATCH("NRM2")
+  }
+
+  //copy
+  void dCopy(Context* ctxt, int64_t n, const double *x,
+                              int64_t incx, double *y, int64_t incy) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::copy(ctxt->queue, n, x, incx, y, incy);
+    ONEMKL_CATCH("COPY")
+  }
+
+  void sCopy(Context* ctxt, int64_t n, const float *x,
+                              int64_t incx, float *y, int64_t incy) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::copy(ctxt->queue, n, x, incx, y, incy);
+    ONEMKL_CATCH("COPY")
+  }
+
+  void zCopy(Context* ctxt, int64_t n, const double _Complex *x,
+                              int64_t incx, double _Complex *y, int64_t incy) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::copy(ctxt->queue, n,
+          reinterpret_cast<const std::complex<double> *>(x), incx,
+          reinterpret_cast<std::complex<double> *>(y), incy);
+    ONEMKL_CATCH("COPY")
+  }
+
+  void cCopy(Context* ctxt, int64_t n, const float _Complex *x,
+                              int64_t incx, float _Complex *y, int64_t incy) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::copy(ctxt->queue, n,
+          reinterpret_cast<const std::complex<float> *>(x), incx,
+          reinterpret_cast<std::complex<float> *>(y), incy);
+    ONEMKL_CATCH("COPY")
+  }
+
   //amax
   void dAmax(Context* ctxt, int64_t n, const double *x,
                             int64_t incx, int64_t *result){
@@ -106,5 +283,158 @@ namespace H4I::MKLShim
     auto status = oneapi::mkl::blas::column_major::iamax(ctxt->queue, n,
                               reinterpret_cast<const std::complex<float> *>(x), incx, result);
     ONEMKL_CATCH("MAX")
+  }
+
+  // amin
+  void dAmin(Context* ctxt, int64_t n, const double *x, int64_t incx, int64_t *result){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::iamin(ctxt->queue, n, x, incx, result);
+    ONEMKL_CATCH("AMIN")
+  }
+  void sAmin(Context* ctxt, int64_t n, const float  *x, int64_t incx, int64_t *result){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::iamin(ctxt->queue, n, x, incx, result);
+    ONEMKL_CATCH("AMIN")
+  }
+  void zAmin(Context* ctxt, int64_t n, const double _Complex *x, int64_t incx, int64_t *result){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::iamin(ctxt->queue, n,
+                            reinterpret_cast<const std::complex<double> *>(x), incx, result);
+    ONEMKL_CATCH("AMIN")
+  }
+  void cAmin(Context* ctxt, int64_t n, const float _Complex *x, int64_t incx, int64_t *result){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::iamin(ctxt->queue, n,
+                            reinterpret_cast<const std::complex<float> *>(x), incx, result);
+    ONEMKL_CATCH("AMIN")
+  }
+  //swap
+  void sSwap(Context* ctxt, int64_t n, float *x, int64_t incx, float *y, int64_t incy){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::swap(ctxt->queue, n, x, incx, y, incy);
+    ONEMKL_CATCH("SWAP")
+  }
+
+  void dSwap(Context* ctxt, int64_t n, double *x, int64_t incx, double *y, int64_t incy){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::swap(ctxt->queue, n, x, incx, y, incy);
+    ONEMKL_CATCH("SWAP")
+  }
+
+  void cSwap(Context* ctxt, int64_t n, float _Complex *x, int64_t incx,
+              float _Complex *y, int64_t incy){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::swap(ctxt->queue, n,
+                            reinterpret_cast<std::complex<float> *>(x), incx,
+                            reinterpret_cast<std::complex<float> *>(y), incy);
+    ONEMKL_CATCH("SWAP")
+  }
+
+  void zSwap(Context* ctxt, int64_t n, double _Complex *x, int64_t incx,
+              double _Complex *y, int64_t incy){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::swap(ctxt->queue, n,
+                            reinterpret_cast<std::complex<double> *>(x), incx,
+                            reinterpret_cast<std::complex<double> *>(y), incy);
+    ONEMKL_CATCH("SWAP")
+  }
+  //rot
+  void sRot(Context* ctxt, int n, float* x, int incx, float* y, int incy,
+              const float c, const float s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rot(ctxt->queue, n, x, incx, y, incy, c, s);
+    ONEMKL_CATCH("ROT")
+  }
+  void dRot(Context* ctxt, int n, double* x, int incx, double* y, int incy,
+              const double c, const double s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rot(ctxt->queue, n, x, incx, y, incy, c, s);
+    ONEMKL_CATCH("ROT")
+  }
+  void cRot(Context* ctxt, int n, float _Complex* x, int incx, float _Complex* y, int incy,
+              const float c, const float _Complex s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rot(ctxt->queue, n,
+                    reinterpret_cast<std::complex<float> *>(x), incx,
+                    reinterpret_cast<std::complex<float> *>(y), incy,
+                    c, static_cast<std::complex<float> >(s));
+    ONEMKL_CATCH("ROT")
+  }
+  void csRot(Context* ctxt, int n, float _Complex* x, int incx, float _Complex* y, int incy,
+              const float c, const float s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rot(ctxt->queue, n,
+                    reinterpret_cast<std::complex<float> *>(x), incx,
+                    reinterpret_cast<std::complex<float> *>(y), incy, c, s);
+    ONEMKL_CATCH("ROT")
+  }
+  void zRot(Context* ctxt, int n, double _Complex* x, int incx, double _Complex* y, int incy,
+              const double c, const double _Complex s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rot(ctxt->queue, n,
+                    reinterpret_cast<std::complex<double> *>(x), incx,
+                    reinterpret_cast<std::complex<double> *>(y), incy,
+                    c, static_cast<std::complex<double> >(s));
+    ONEMKL_CATCH("ROT")
+  }
+  void zdRot(Context* ctxt, int n, double _Complex* x, int incx, double _Complex* y, int incy,
+              const double c, const double s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rot(ctxt->queue, n,
+                    reinterpret_cast<std::complex<double> *>(x), incx,
+                    reinterpret_cast<std::complex<double> *>(y), incy, c, s);
+    ONEMKL_CATCH("ROT")
+  }
+  //rotg
+  void sRotg(Context* ctxt, float* a, float* b, float* c, float* s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rotg(ctxt->queue, a, b, c, s);
+    ONEMKL_CATCH("ROTG")
+  }
+  void dRotg(Context* ctxt, double* a, double* b, double* c, double* s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rotg(ctxt->queue, a, b, c, s);
+    ONEMKL_CATCH("ROTG")
+  }
+  void cRotg(Context* ctxt, float _Complex* a, float _Complex* b, float* c, float _Complex* s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rotg(ctxt->queue,
+                                                        reinterpret_cast<std::complex<float> *>(a),
+                                                        reinterpret_cast<std::complex<float> *>(b), c,
+                                                        reinterpret_cast<std::complex<float> *>(s));
+    ONEMKL_CATCH("ROTG")
+  }
+  void zRotg(Context* ctxt, double _Complex* a, double _Complex* b, double* c, double _Complex* s){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rotg(ctxt->queue,
+                                                        reinterpret_cast<std::complex<double> *>(a),
+                                                        reinterpret_cast<std::complex<double> *>(b), c,
+                                                        reinterpret_cast<std::complex<double> *>(s));
+    ONEMKL_CATCH("ROTG")
+  }
+  //rotm
+  void sRotm(Context* ctxt, int64_t n, float *x, int64_t incx,
+              float *y, int64_t incy, float* param) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rotm(ctxt->queue, n, x, incx, y, incy, param);
+    ONEMKL_CATCH("ROTM")
+  }
+  void dRotm(Context* ctxt, int64_t n, double *x, int64_t incx,
+              double *y, int64_t incy, double* param){
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rotm(ctxt->queue, n, x, incx, y, incy, param);
+    ONEMKL_CATCH("ROTM")
+  }
+
+  //rotmg
+  void sRotmg(Context* ctxt, float *d1, float *d2, float *x1, float y1, float* param) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rotmg(ctxt->queue, d1, d2, x1, y1, param);
+    ONEMKL_CATCH("ROTMG")
+  }
+  void dRotmg(Context* ctxt, double *d1, double *d2, double *x1, double y1, double* param) {
+    ONEMKL_TRY
+    auto status = oneapi::mkl::blas::column_major::rotmg(ctxt->queue, d1, d2, x1, y1, param);
+    ONEMKL_CATCH("ROTMG")
   }
 }// end of namespace
