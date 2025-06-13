a24c6b2d62a726fe1504ede9f4301341d5c13fb7
Paulius Velesko <pvelesko@pglc.io>
2025-06-13 17:11:01 +0300

    remove FORCE FLUSH

--- include/h4i/mklshim/mklshim.h
+++ include/h4i/mklshim/mklshim.h
@@ -8,30 +8,6 @@
 #include "h4i/mklshim/onemklsolver.h"
 #include "h4i/mklshim/onemklblas.h"
 
-// This is a workaround to flush MKL submissions into Level-zero queue,
-// using unspecified but guaranteed behavior of intel-sycl runtime.
-// Once SYCL standard committee approves sycl::queue::flush() we will change the macro to use the same
-// FIXED: Handle OneAPI 2024.2.2 backend mismatch issue where OneMKL returns events with wrong backend
-#define __FORCE_MKL_FLUSH__(cmd)                                \
-    try {                                                       \
-        if (currentBackend == opencl) {                         \
-            try {                                               \
-                sycl::get_native<sycl::backend::opencl>(cmd);         \
-            } catch (const sycl::exception& e) {                \
-                /* OneMKL 2024.2.2 bug: status has wrong backend */ \
-                sycl::get_native<sycl::backend::ext_oneapi_level_zero>(cmd); \
-            }                                                   \
-        } else {                                                \
-            try {                                               \
-                sycl::get_native<sycl::backend::ext_oneapi_level_zero>(cmd); \
-            } catch (const sycl::exception& e) {                \
-                /* Fallback to OpenCL if Level Zero fails */   \
-                sycl::get_native<sycl::backend::opencl>(cmd);         \
-            }                                                   \
-        }                                                       \
-    } catch (const sycl::exception& e) {                        \
-        /* If both fail, skip the flush (best effort) */       \
-    }
 
 #define ONEMKL_TRY \
     if(ctxt == nullptr) { \
@@ -64,7 +40,6 @@
 
 #define ONEMKL_CATCH(msg) \
     status.wait_and_throw(); \
-    __FORCE_MKL_FLUSH__(status); \
     __CATCH__(msg)
 
 #define ONEMKL_CATCH_NO_FLUSH(msg) \
